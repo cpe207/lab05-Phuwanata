8d04180c3446d910ff9379f96d37fefedd2b1587
phuwanat taratipprai <phuwanat@cmu.ac.th>
2024-07-06 18:52:16 +0700

    s

--- q1.ts
+++ q1.ts
@@ -1,34 +1,37 @@
-// define interface for Hero and Shop objects
 interface Hero {
-  /* Your code here */
+  item : string[];
+  gold : number;
 }
 
 interface Shop {
-  /* Your code here */
+  item : string;
+  price : number;
 }
 
-// assign interface/type to the function definition properly
-function buyItem(hero, shop) {
-  /* Your code here */
+function buyItem(hero : Hero, shop : Shop) {
+  if(hero.gold > shop.price){
+    hero.item.push(shop.item);
+    hero.gold -= shop.price;
+  }
+  return hero;
 }
 
-//Test cases : assign proper type/interface to all objects
-const hero1 = {
-  items: ["sword", "potion"],
+let hero1: Hero = {
+  item: ["sword", "potion"],
   gold: 50,
 };
 
-const shop1 = {
+let shop1 : Shop = {
   item: "armor",
   price: 20,
 };
 
-const hero2 = {
-  items: ["sword", "potion"],
+let hero2 : Hero = {
+  item: ["sword", "potion"],
   gold: 50,
 };
 
-const shop2 = {
+let shop2 : Shop= {
   item: "legendary armor",
   price: 200,
 };
@@ -37,3 +40,7 @@ console.log(buyItem(hero1, shop1));
 console.log(buyItem(hero2, shop2));
 
 module.exports = buyItem;
+/*
+รหัสนักศึกษา : 660610786
+ชื่อ-สกุล: นายภูวนาถ ธาราทิพย์ไพร
+*/
